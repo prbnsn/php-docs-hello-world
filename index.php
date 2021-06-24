5c04e405cee1dbfb968baa6c1ec21d7c5a723478
Pete Robinson <pete.robinson@gmail.com>
2021-06-24 14:05:24 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello World! CI test!";
+$tags = explode(', ', "AI:EXAMP849U:5196476, Ready to Pick., rerun-order, Updated by VL OMNI.");
+$tag = 'rerun-order';
+if (in_array($tag, $tags)) {
+  echo("found tag");
+else {
+  echo("not found");
+}
+var_dump( explode( ', ', $tags ) );
+?>
